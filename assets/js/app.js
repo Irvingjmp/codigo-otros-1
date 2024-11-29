e8e29772fdaa298ee1bea16ef0a636c512bd3691
Irvingjmp <irvingjmp@gmail.com>
2024-11-29 10:47:25 -0600

    Codigo 1 revisado y resuelto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,34 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+
+// const $n = document.querySelector('name'); Se debe referenciar a la clase
+// const $b = document.querySelector('#blog'); Se referencian a ID pero es una clase
+
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { //Debe ser una función async
+  //Faltaba un try y un catch
+  try{
+    $n.textContent = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    const data =  await response.json();
+
+    console.log(data);
+    $n.textContent = `${data.name}`; //Se usan backticks
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  } catch(err){
+    handleError(err)
+  }
 }
+  
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` //Variable n mal definida
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
